fde0622c3157d75e61d578e7e3b785254868e8d5
Victor Jose <vepncor@gmail.com>
2024-01-18 12:46:07 +0100

    c

--- ejemploJosn/code.js
+++ ejemploJosn/code.js
@@ -1,1 +1,17 @@
-document.addEventListener("DOMContentLoaded",()=>)
+document.addEventListener("DOMContentLoaded",()=>{
+    const toma =new XMLHttpRequest();
+    toma.open("GET","data/mascotas.json");
+
+    toma.send();
+    toma.responseType="json";
+    toma.addEventListener("load",()=>{
+        if (toma.readyState == 4 && toma.status == 200) {
+        const datos=toma.response;
+        console.log(datos);
+        }else{
+            console.log("Hubo un error");
+        }
+    })
+
+
+})
